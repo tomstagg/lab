d8db48e7fe49d76b60c3ad2c76ef23ede06e1c41
Tom Stagg <tomstagg@gmail.com>
2015-12-07 16:44:07 +0000

    update gitignore, add ch2 case study

---
+++ ch2-cs-notebook/notebook.py
@@ -0,0 +1,59 @@
+import datetime
+
+# stores the next available id for all new notes
+last_id = 0
+
+
+class Note:
+    """ Represents a note in the notebook. Match against a string in searches and stores tag for each note
+    """
+
+    def __init__(self, memo, tags=''):
+        """
+        initalise a note with a memo and optionally space-seperated tags.
+        Automatically set note's creation date and a unique id.
+        """
+
+        self.memo = memo
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global last_id
+        last_id += 1
+        self.id = last_id
+
+    def match(self, filter):
+        """determine if this note matches the filter text. Return True if it matches.
+        """
+        return filter in self.memo or filter in self.tags
+
+
+class Notebook:
+    """Represents a collection of notes that can be tagged, modified, and searched."""
+    def __init__(self):
+        """Initalise a notebook with an empty list"""
+        self.notes = []
+
+    def new_note(self, memo, tags=''):
+        """create a new note and add it to the list."""
+        self.notes.append(Note(memo, tags))
+
+    def _find_note(self, note_id):
+        """Locate the note with the given id """
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+        return None
+
+    def modify_memo(self, note_id, memo):
+        """  Find the note with the given id and change its memo to the given value"""
+        self._find_note(note_id).memo = memo
+
+    def modify_tags(self, note_id, tags):
+        """ Find the note with the given id and change its tags to the given value"""
+        self._find_note(note_id).tags = tags
+
+    def search(self, filter):
+        """ Find all notes that match the given filter string.
+        """
+        return [note for note in self.notes if note.match(filter)]
+
